c0bceba050faa55ec97e5edefd5445194dd69951
EXT02D9773 <ayhan.murat@consultant.turkcell.com.tr>
2019-07-17 09:25:39 +0300

    GAMING PLATFORM API REDIS INTEGRATION AND REFINEMENTS

--- gamingplatform-api/src/main/java/com/turkcell/playcell/gamingplatform/SpringBootAppStarter.java
+++ gamingplatform-api/src/main/java/com/turkcell/playcell/gamingplatform/SpringBootAppStarter.java
@@ -4,14 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import com.turkcell.playcell.gamingplatform.api.controller.GameListController;
-import com.turkcell.playcell.gamingplatform.api.controller.UserApiController;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
--- gamingplatform-common/src/main/java/com/turkcell/playcell/gamingplatform/common/config/CommonApplicationProperties.java
+++ gamingplatform-common/src/main/java/com/turkcell/playcell/gamingplatform/common/config/CommonApplicationProperties.java
@@ -1,5 +1,7 @@
 package com.turkcell.playcell.gamingplatform.common.config;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +19,12 @@ public class CommonApplicationProperties {
 
     @Value("${spring.redis.password}")
     private String redisPassword;
+    
+    @Value("${spring.redis.sentinel.master}")
+	String sentinelMaster;
+    
+	@Value(value = "${spring.redis.sentinel.nodes}")
+	Set<String> sentinelNodes;
     
     @Value("${cache.expirations.time}")
     private Integer cacheExpirationTime;
